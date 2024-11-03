06263f7d6e7bb740467b2d38391704bab401d009
Dhoritri <ishrak.alam32@gmail.com>
2024-11-03 18:45:39 +0600

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
 import logo from "../assets/DhoritriLogo.png"
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub} from "react-icons/fa"
-import { FaTwitter } from "react-icons/fa"
-import { FaInstagram} from "react-icons/fa"
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa"
+
 function Navbar() {
   return (
     <>
-    <nav className=" mb-20 flex items-center justify-between py-6">
+      <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-32" src={logo} alt="Dhoritri Logo" />
-
+          <img className="mx-2 w-32" src={logo} alt="Dhoritri Logo" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin/>
-            <FaGithub/>
-            <FaTwitter/>
-            <FaInstagram/>
+          <a href="https://www.linkedin.com/in/ishrak-alam-0920b71b7/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/Dhoritri" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="https://www.instagram.com/ishrak7o7/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
         </div>
-    </nav>
-    </>)
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,41 +1,114 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { SiJavascript, SiPython, SiTailwindcss, SiMysql, SiUnity, SiDotnet, SiCsharp, SiBootstrap } from "react-icons/si";
-
+import {
+  SiJavascript,
+  SiPython,
+  SiTailwindcss,
+  SiMysql,
+  SiUnity,
+  SiDotnet,
+  SiCsharp,
+  SiBootstrap,
+} from "react-icons/si";
+import { motion } from "framer-motion";
 const Technologies = () => {
+  const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+      y: [10, -10],
+      transition: {
+        duration: duration,
+        ease: "linear",
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    },
+  });
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 className="my-20 text-center text-4xl">Technologies</h1>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.h1 
+      whileInView={{opacity:1,y:0}}
+      initial={{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+      className="my-20 text-center text-4xl">Technologies</motion.h1>
+      <motion.div whileInView={{opacity:1, x:0}}
+       initial={{opacity:0,x:-100}}
+       transition={{duration:1.5}}
+      className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiJavascript className="text-7xl text-yellow-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiPython className="text-7xl text-blue-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiTailwindcss className="text-7xl text-sky-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMysql className="text-7xl text-blue-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiUnity className="text-7xl text-black" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiDotnet className="text-7xl text-green-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiCsharp className="text-7xl text-blue-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiBootstrap className="text-7xl text-purple-600" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
-}
+};
 
 export default Technologies;
